8d789d19cf4eb480f3e3118b5723c1dcd5acdac7
Sandip Pal <sandip@asteria.co.in>
2018-01-08 15:46:21 +0530

    Instals React App & adds configaration

---
+++ asteria-webui/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins" : [
+        "react",
+        "jsx-a11y",
+        'import'
+    ],
+    "env": {
+        "browser": true
+      },
+      "globals": {
+        "fetch": true
+    },
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "no-use-before-define": 0,
+        "no-undef": 0,
+        "linebreak-style": ["error", "unix"],
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/forbid-prop-types": ["off"],
+        "jsx-a11y/no-autofocus": [ 0, {
+            "ignoreNonDOM": true
+        }],
+    }
+};
---
+++ asteria-webui/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.css$/,  
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: 'assets/',
+              outputPath: 'images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 95
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      },
+
+    ]
+  },
+  devServer: {
+        contentBase: './public',
+        hot: true,
+        open: true,
+        port: 9000,
+        historyApiFallback: true
+  }
+};
+
+module.exports = config;
